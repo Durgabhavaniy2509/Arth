513b34529dff048203bcb7c9d1f3f4e62bea8655
Durgabhavaniy2509 <db8538660@gmail.com>
2024-11-26 07:00:18 +0530

    countup is added for the hero-stat

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './Home.css'
+import CountUp from 'react-countup'
 
 const Home = () => {
   return (
@@ -8,17 +9,23 @@ const Home = () => {
       <div className=' flexCenter hero-stats'>
           <div className='flexColCenter hero-stat'>
             <img src="\Icons\completed-task.png" alt="" />
-            <span className='orangeText'>300+</span>
+            <span className='orangeText'>
+              <CountUp start= {250} end= {300} duration={3}/>+
+            </span>
             <span className='primaryText'>Projects Completed</span>
           </div>
           <div className='flexColCenter hero-stat '>
             <img src="\Icons\rating.png" alt="" />
-            <span className='orangeText'>300+</span>
+            <span className='orangeText'>
+              <CountUp start= {250} end= {300} duration={3}/>+
+              </span>
             <span className='primaryText'>Experts</span>
           </div>
           <div className='flexColCenter hero-stat'>
             <img src="\Icons\countries.png" alt="" />
-            <span className='orangeText'>7</span>
+            <span className='orangeText'>
+              <CountUp start= {3} end= {7} duration={3}/>
+            </span>
             <span className='primaryText'>Countries</span>
           </div>
   
