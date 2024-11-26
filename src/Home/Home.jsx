9fa470d58f1e15914d64bc6c1adb9d559367efbf
Durgabhavaniy2509 <db8538660@gmail.com>
2024-11-26 07:19:22 +0530

    html for about us is completed

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -5,7 +5,16 @@ import CountUp from 'react-countup'
 const Home = () => {
   return (
     <section>
-      <div>
+      <div className=" flexCenter1 paddings innerWidth hero-aboutUs">
+        <div className='hero-left'>
+          <img src="\Images\Aqua Raffles hotel.avif" alt="" />
+        </div>
+        <div className='hero-right'>
+            <div>ABOUT US</div>
+            <div>At Arth, we are committed to revolutionizing the construction industry by <br />focusing on process efficiency and collaborative problem-solving. Our teams <br />are driven by the belief that successful projects come from well-executed plans <br /> and transparent communication. Whether working on residential, commercial, or <br /> infrastructure projects, we empower our clients with innovative, data-driven <br />solutions that ensure seamless execution from start to finish. With a global <br /> presence and a passion for excellence, Arth is redefining the construction <br />landscape, one project at a time.</div>
+        </div>
+      </div>
+
       <div className=' flexCenter hero-stats'>
           <div className='flexColCenter hero-stat'>
             <img src="\Icons\completed-task.png" alt="" />
@@ -28,9 +37,7 @@ const Home = () => {
             </span>
             <span className='primaryText'>Countries</span>
           </div>
-  
-      </div>
-      </div>
+       </div>
     </section>
   )
 }
