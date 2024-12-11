b2903d7496ba1dc813216e596a764743b09c88ad
Durgabhavaniy2509 <db8538660@gmail.com>
2024-12-12 02:40:02 +0530

    Header becomes responsive

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,71 +1,59 @@
-import React from 'react'
-import { Component } from 'react';
+import { useState } from 'react';
 import './Header.css';
-import { Link, NavLink } from 'react-router-dom';
-import Home from '../Home/Home';
-import { AlignJustify, X } from 'lucide-react';
 
-const Header = () => {
-  var mainListDiv = document.getElementById("mainListDiv"),
-    mediaButton = document.getElementById("mediaButton");
+function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
 
-mediaButton.onclick = function () {
-    
-    "use strict";
-    
-    mainListDiv.classList.toggle("show_list");
-    mediaButton.classList.toggle("active");
-    
-};
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <nav class="nav">
-        <div class="navbar-container">
-            <div class="logo">
-                <a href="#">YourLogo</a>
-            </div>
-            <div class="main_list" id="mainListDiv">
-                <ul>
-                    <li><a href="#">Home</a></li>
-                    <li><a href="#">About</a></li>
-                    <li><a href="#">my work</a></li>
-                    <li><a href="#">disegn</a></li>
-                    <li><a href="#">dcontact</a></li>
+    <nav className="navbar">
+      <div className="navbar-container">
+        <div className="navbar-brand">
+          <a href="/Home" className="logo">< img src="\Images\Arth.webp" alt="" width={100} /></a>
+        </div>
+
+        {/* Desktop Navigation */}
+        <div className="desktop-nav">
+          <ul className="nav-links">
+            <li><a href="/Home">Home</a></li>
+            <li><a href="/OurFirm">Our Firm</a></li>
+            <li><a href="/Projects">Projects</a></li>
+            <li><a href="/Resources">Resources</a></li>
+            <li><a href="/Contact">Contact</a></li>
+           </ul>
+        </div>
+
+        {/* Mobile Menu Button */}
+        <button className="mobile-menu-button" onClick={toggleMenu}>
+          <span className={`hamburger ${isOpen ? 'open' : ''}`}>
+            <span></span>
+            <span></span>
+            <span></span>
+          </span>
+        </button>
+
+        {/* Mobile Navigation */}
+
+          <div className={`mobile-menu ${isOpen ? 'open' : ''}`}>
+            <div className="mobile-menu-content">
+                <ul className="nav-links">
+                  <li><a href="/Home">Home</a></li>
+                  <li><a href="/OurFirm">Our Firm</a></li>
+                  <li><a href="/Projects">Projects</a></li>
+                  <li><a href="/Resources">Resources</a></li>
+                  <li><a href="/Contact">Contact</a></li>
                 </ul>
             </div>
-            <div class="media_button">
-                <button class="main_media_button" id="mediaButton">
-                    <span></span>
-                    <span></span>
-                    <span></span>
-                </button>
-            </div>
-        </div>
-      </nav>
-  )
+        </div>        
+      </div>
+    </nav>
+  );
 }
 
-export default Header
-
-
-      // <nav className='h-wrapper'>
-      //   <div className='flexCenter1 paddings innerWidth h-container'>
-      //     <div className='h-logo'>
-      //       < img src="\Images\Arth.webp" alt="" width={100} />
-      //     </div>
-      //     <div className='flexCenter h-menu'>
-      //       <Link to='./Home' className='active'>Home</Link>
-      //       <Link to='./OurFirm' >Our Firm</Link>
-      //       <Link to='./Services' >Services</Link>
-      //       <Link to='./Projects' >Projects</Link>
-      //       <Link to='./Resources' >Resources</Link>
-      //       <Link to='./Contact' >Contact</Link>
-      //     </div>
-
-      //     <div id="mobile">
-      //       <span id='bar' className={this.state.clicked? <AlignJustify/>: <X/>}></span>
-      //     </div>
-      //   </div>
-      // </nav>
+export default Navbar;
 
       
 
